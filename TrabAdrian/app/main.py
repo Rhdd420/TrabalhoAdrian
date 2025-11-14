4d71ee22d1b5a96552caef40c69cfe3aaaf31756
rhdd420 <rayanbola11.com>
2025-11-13 21:03:00 -0300

    Commit inicial a partir da cópia local

---
+++ TrabAdrian/app/main.py
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Olá, Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Olá, Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+>>>>>>> 09865ba (Add files via upload)
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Olá, Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+>>>>>>> 4c2cc360af456d665ddbe735f6baa3c0e612940a
